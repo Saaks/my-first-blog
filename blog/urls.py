8ef0fe3af5ebc5e9511704a37c7842c13faa5c8a
acesaak <acesaak@gmail.com>
2020-06-20 11:56:00 +0900

    reply

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 ]
