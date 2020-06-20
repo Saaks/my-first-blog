a8fff239bd07e70d8b90cb7876d167657a97bb9c
acesaak <acesaak@gmail.com>
2020-06-20 19:29:14 +0900

    photo

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/orig')
+    filtered_image = models.ImageField(upload_to='uploads/%Y/%m/%d/filtered')
+    content = models.TextField(max_length=500, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        self.filtered_image.delete()
+        super(Photo, self).delete(*args, **kwargs)
